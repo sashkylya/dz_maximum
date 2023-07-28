0bc8b976b0283ad2ba263b6e412e2e41e5d34e7d
sashkylya <aleksandrsimonov962@gmail.com>
2023-07-28 14:32:11 +0300

    advertisement

--- advertisement/app_advertisement/models.py
+++ advertisement/app_advertisement/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField('Название', max_length=120)
+    descriptions = models.TextField('Описание')
+    price = models.DecimalField('Цена',max_digits=9,decimal_places=2)
+    trades = models.BooleanField('Торг',help_text='ЕСли хотим торговаться')
+    date_now = models.DateField('Создание дата', auto_now_add= True)
+    date_update = models.DateField('Обновление дата', auto_now= True)
+
+    db_table = 'advertisements'
+
+    def __str__(self):
+        return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
