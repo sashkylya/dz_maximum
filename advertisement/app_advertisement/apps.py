dc2af9776eb0a12e0979dc501e03c0cfa3d8ac0d
sashkylya <95548609+sashkylya@users.noreply.github.com>
2023-08-17 21:20:22 +0300

    advertisement

--- advertisement/app_advertisement/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdvertisementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_advertisement'
-
-    verbose_name = 'Объявления'
--- advertisement/app_lesson_4/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def dz(request):
-    return HttpResponse('Домашка по 4 Занятию')
